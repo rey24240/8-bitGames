2010dc60fac67c9b0a5a3877ee94e250f3ca4471
rey24240 <btwgoon24@gmail.com>
2025-03-24 06:07:46 +0000

    initial commit

--- static/assets/uv/js/index.js
+++ static/assets/uv/js/index.js
@@ -1,29 +1,28 @@
-const form = document.querySelector("form");
+const form = document.querySelector(".searchbar");
 const input = document.querySelector("input");
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  window.navigator.serviceWorker
-    .register("./sw.js", {
-      scope: __uv$config.prefix,
-    })
-    .then(() => {
-      let url = input.value.trim();
-      if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
-      else if (!(url.startsWith("https://") || url.startsWith("http://")))
-        url = "http://" + url;
-      localStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
-      //remove.forEach(element => {
-        //element.remove();
-      //});
-      location.href = "/portal";
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    window.navigator.serviceWorker
+      .register("/assets/uv/sw.js", {
+        scope: __uv$config.prefix,
+      })
+      .then(() => {
+        let url = input.value.trim();
+        if (!isUrl(url)) url = "https://www.startpage.com/do/dsearch?q=" + url;
+        else if (!(url.startsWith("https://") || url.startsWith("http://")))
+          url = "http://" + url;
+        sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+        location.href = "!";
+      });
+  });
 });
 
 function isUrl(val = "") {
   if (
     /^http(s?):\/\//.test(val) ||
-    (val.includes(".") && val.substr(0, 1) !== " ")
+    (val.includes(".") && val.slice(0, 1) !== " ")
   )
     return true;
   return false;
